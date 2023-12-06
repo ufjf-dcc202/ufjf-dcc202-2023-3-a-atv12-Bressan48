43faa806bfc9c6fb987ea5823d83cb64cc9d26b0
Bernardo Mattos <bressan.mattos@estudante.ufjf.br>
2023-12-06 20:29:02 -0300

    complementa leFormulario e adiciona evento

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 //main.js
 
+document.entrada.addEventListener('submit', leFormulario);
 leFormulario();
 
-function leFormulario(){
-    const fruta = documento.entrada.fruta.value;
-    const quantidade = document.entrada.fruta.value;
+function leFormulario(event){
+    event.preventDefault();
+    const fruta = document.entrada.fruta.value;
+    const quantidade = document.entrada.quantidade.value;
 
-    console.log(fruta, quantidade);
+    console.log(`fruta: ${fruta} qtd: ${quantidade}`);
 }

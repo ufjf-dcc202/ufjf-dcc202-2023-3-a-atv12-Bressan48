959e34abc00b6ee94b97233c46998a5731d013c0
Bernardo Mattos <bressan.mattos@estudante.ufjf.br>
2023-12-06 20:22:17 -0300

    cria form e a funcao leFormulario

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
 //main.js
 
+leFormulario();
+
+function leFormulario(){
+    const fruta = documento.entrada.fruta.value;
+    const quantidade = document.entrada.fruta.value;
+
+    console.log(fruta, quantidade);
+}
